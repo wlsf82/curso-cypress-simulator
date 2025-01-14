c573c58ee5d7363f789e70304d4513f2999cdf8e
Walmyr <wlsf82@gmail.com>
2025-01-14 12:30:37 +0100

    Configure cypress-split

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,15 @@
 const { defineConfig } = require("cypress")
 
+const cypressSplit = require("cypress-split")
+
 module.exports = defineConfig({
   viewportHeight: 1024,
   viewportWidth: 1700,
   e2e: {
     fixturesFolder: false,
+    setupNodeEvents(on, config) {
+      cypressSplit(on, config)
+      return config
+    }
   },
 })
