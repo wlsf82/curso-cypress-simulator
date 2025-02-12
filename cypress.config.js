311bcff9344b6a17a465ff2b0ef6500573cc0fa8
Pablo Nogueira <pablottoni@gmail.com>
2025-02-11 20:41:35 -0400

    create test cases

---
+++ cypress.config.js
@@ -0,0 +1,9 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  viewportHeight: 1024,
+  viewportWidth: 1700,
+  e2e: {
+    fixturesFolder: false,
+  },
+})
---
+++ cypress/e2e/cypress-simulator.cy.js
@@ -0,0 +1,73 @@
+describe('Cypress simulator', () => {
+  it('Success', () => {
+
+  })
+  
+  it('Error: invalid cypress command ', () => {
+    
+  });
+
+  it('Warning', () => {
+    
+  });
+
+  it('Error: valid command withou parentheses', () => {
+    
+  });
+
+  it('Help', () => {
+    
+  });
+
+  it('Maximize/minimize', () => {
+    
+  });
+
+  it('Logout', () => {
+    
+  });
+
+  it('Show and hide logout button', () => {
+    
+  });
+
+  it('Running... state', () => {
+    
+  });
+
+  it('Accept cookies', () => {
+    
+  });
+
+  it('Decline cookies', () => {
+    
+  });
+
+  it('Captcha button states', () => {
+    
+  });
+
+  it('Captcha error', () => {
+    
+  });
+
+  it('Run button - enable/disable states', () => {
+    
+  });
+
+  it('Reset textarea on logout and login', () => {
+    
+  });
+
+  it('Disable run button on logout and login', () => {
+    
+  });
+
+  it('Reset output on logout an login', () => {
+    
+  });
+
+  it('No cookings banner on the login page', () => {
+    
+  });
+})
